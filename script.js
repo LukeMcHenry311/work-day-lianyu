fba68c41d19fa5b49949b6a22acd7fa1e099fa3a
McHenry <lukemchenry311@gmail.com>
2021-10-09 19:20:56 -0600

    change hours to usable

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 var today = moment().format("[today is] dddd MMM Do[,] YYYY");
-var hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+var hours = ["2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm"]
+var time = moment().format("Ha");
+var currentTime = moment(time, "Ha");
 
 shipHours = function() {
     $.each(hours, function(i) {
 
+        var scheme = moment(hours[i], "Ha");
+
         let hourBlock = $('<li>').addClass("list-group-item").text(hours[i]);
 
+        if (moment(currentTime).isAfter(scheme)) {
+            $(hourBlock).addClass("past");
+        } if (moment(currentTime).isSame(scheme)) {
+            $(hourBlock).addClass("present");
+        } if (moment(currentTime).isBefore(scheme)) {
+            $(hourBlock).addClass("future");
+        }
+
         $('.list-group').append(hourBlock);
+
+        $('.list-group').addClass("big");
     })
 };
 

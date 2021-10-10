21bba9cb1a8a44efcb0a6b4f4579dcc46433663c
McHenry <lukemchenry311@gmail.com>
2021-10-09 19:00:09 -0600

    timeblocks added

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hiiii");
+var today = moment().format("[today is] dddd MMM Do[,] YYYY");
+var hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+
+shipHours = function() {
+    $.each(hours, function(i) {
+
+        let hourBlock = $('<li>').addClass("list-group-item").text(hours[i]);
+
+        $('.list-group').append(hourBlock);
+    })
+};
+
+updateToday = function() {
+    $('#currentDay').text(today);
+};
+
+updateToday();
+
+shipHours();

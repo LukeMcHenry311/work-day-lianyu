6ebfb3b95a578022e0977744c6a107a5cf42ffb2
McHenry <lukemchenry311@gmail.com>
2021-10-10 05:30:46 -0600

    working text area and save button added

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
 var today = moment().format("[today is] dddd MMM Do[,] YYYY");
-var hours = ["2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm"]
+var hours = ["2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm"];
 var time = moment().format("Ha");
 var currentTime = moment(time, "Ha");
 
-shipHours = function() {
-    $.each(hours, function(i) {
+shipHours = function () {
+  $.each(hours, function (i) {
+    var scheme = moment(hours[i], "Ha");
+    var id = "#" + i.toString();
 
-        var scheme = moment(hours[i], "Ha");
+    var hourContainer = $("<li>").attr("id", i).addClass("row time-block");
+    $(".list-group").append(hourContainer);
 
-        let hourBlock = $('<li>').addClass("list-group-item").text(hours[i]);
+    let hourBlock = $("<li>").addClass("list-group-item").text(hours[i]);
+    $(id).append(hourBlock);
 
-        if (moment(currentTime).isAfter(scheme)) {
-            $(hourBlock).addClass("past");
-        } if (moment(currentTime).isSame(scheme)) {
-            $(hourBlock).addClass("present");
-        } if (moment(currentTime).isBefore(scheme)) {
-            $(hourBlock).addClass("future");
-        }
+    var texty;
 
-        $('.list-group').append(hourBlock);
+    if (moment(currentTime).isAfter(scheme)) {
+      texty = $("<textarea>").addClass("col-8 description past").text("text");
+      $(id).append(texty);
+    }
+    if (moment(currentTime).isSame(scheme)) {
+      texty = $("<textarea>").addClass("col-8 present").text("text");
+      $(id).append(texty);
+    }
+    if (moment(currentTime).isBefore(scheme)) {
+      texty = $("<textarea>").addClass("col-8 future").text("text");
+      $(id).append(texty);
+    }
 
-        $('.list-group').addClass("big");
-    })
+    var saveDiv = $("<i>").addClass("col-md-2 saveBtn").text("save");
+    $(id).append(saveDiv);
+
+    $(".list-group").addClass("big");
+  });
 };
 
-updateToday = function() {
-    $('#currentDay').text(today);
+updateToday = function () {
+  $("#currentDay").text(today);
 };
 
 updateToday();
